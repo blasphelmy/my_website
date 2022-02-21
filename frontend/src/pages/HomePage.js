260e1f3d420acf9b66dcd4ddfb24f262d5e0ee5d
Hikmaasiraj <64751446+Hikmaasiraj@users.noreply.github.com>
2022-02-20 19:29:09 -0800

    Update HomePage.js

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -20,7 +20,7 @@ function HomePage() {
         {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
+              <img src={product.images} alt={product.name} />
             </Link>
             <div className="product-info">
               <Link to={`/product/${product.slug}`}>
